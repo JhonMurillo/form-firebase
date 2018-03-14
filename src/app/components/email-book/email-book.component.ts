1872a630ccd7f418ad79b2917e95e7c46c513187
Jhon Mario Murillo Cordoba <jhonmurillo2014@gmail.com>
2018-03-13 23:30:37 -0500

    Create module of email-book

--- src/app/components/email-book/email-book.component.ts
+++ src/app/components/email-book/email-book.component.ts
@@ -6,6 +6,9 @@ import { moveIn, fallIn } from '../../router.animations';
 import { UserServiceService } from '../../services/user-service.service';
 import { User } from '../../classes/user';
 import { Observable } from 'rxjs/Observable';
+declare var jQuery: any;
+declare var $: any;
+import { toast } from 'angular2-materialize';
 
 @Component({
   selector: 'app-email-book',
@@ -16,9 +19,11 @@ export class EmailBookComponent implements OnInit {
   state: string = '';
   error: any;
   success: any;
-  email;
+  emailCurrent;
   title: string = 'Accounts';
   users: Observable<Array<User>> = null;
+  usersAux: Map<string, boolean>;
+  elementInput: Object;
 
   authState: any = null;
   constructor(public af: AngularFireAuth, private router: Router, private db: AngularFireDatabase, private userService: UserServiceService) {
@@ -27,9 +32,43 @@ export class EmailBookComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-    this.users = this.userService.users;
-    console.log(this.userService.users);
+  onLoadAutoComplete(context) {
+    $('input.autocomplete').autocomplete({
+      data: this.elementInput,
+      limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+      onAutocomplete: function (val) {
+        context.elementInput;
+        var verified = context.usersAux.get(val);
+        if (!verified) {
+          toast('!User Not Verified ' + val, 3000, 'rounded')
+          $('input.autocomplete').val('');
+        }
+      },
+      minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+    },this);
   }
+
+  ngOnInit() {
+    this.emailCurrent = this.af.auth.currentUser.email;
+    this.users = this.userService.users;
+    this.elementInput = {};
+    this.usersAux = new Map();
+    var index = 0;
+    this.users.forEach(element => {
+      element.forEach(user => {
+        if (user.email !== this.emailCurrent) {
+          this.elementInput[user.email] = null;
+          this.usersAux.set(user.email, user.name === null || user.name === undefined ? false : true);
+          index++;
+        }
+      });
+      this.onLoadAutoComplete(this);
+    });
+
+  }
+
+
+
+
 
 }
